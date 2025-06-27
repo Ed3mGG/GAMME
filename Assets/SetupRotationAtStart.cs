02f0dc1720590f683b5a73db6866167aa50a8951
Victor <edemgggames@gmail.com>
2025-06-27 10:44:31 +0200

    Fixed script SetupRotationAtStart for scene "Détente" (AR2)

--- Assets/SetupRotationAtStart.cs
+++ Assets/SetupRotationAtStart.cs
@@ -1,18 +1,35 @@
+using System.Collections;
 using UnityEngine;
 
 public class SetupRotationAtStart : MonoBehaviour
 {
 
-    [SerializeField] private Camera m_Camera;
-    [SerializeField] private Vector3 m_Rotation;
-    [SerializeField] private Vector3 m_Setup;
+    [SerializeField] private Camera _Camera;
+    [SerializeField] private GameObject _Setup;
+    [SerializeField] private float _delayBeforeRotation;
+    
+    private Vector3 _SetupPosition;
+    
+    Coroutine coroutine;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if (m_Camera != null)
+        _SetupPosition =  _Setup.transform.position;
+        if (_Camera != null)
         {
-            //m_SetupTransform.position = m_Camera.transform.position;
-            
+            StartCoroutine(DelayBeforeRotation(_delayBeforeRotation));
         }
+    }
+
+    private IEnumerator DelayBeforeRotation(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        _Setup.transform.position = _Camera.transform.position + _Camera.transform.forward;
+        var angles = _Setup.transform.eulerAngles;
+        angles.y = _Camera.transform.rotation.eulerAngles.y;
+        _Setup.transform.eulerAngles = angles;
+        var vector3 = _Setup.transform.position;
+        vector3.y = _SetupPosition.y;
+        _Setup.transform.position = vector3;
     }
 }
